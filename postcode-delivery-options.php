f04a2ee4ac80a97806640d357a0bb5e964b758c3
Terence Denning <terrencedenning@gmail.com>
2020-04-06 14:03:01 +1000

    initial

---
+++ postcode-delivery-options.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Postcode Delivery Options
+Description: Add delivery day options to specified postcodes
+Version: 1.0.0
+Contributors: tdenning
+Author: Terence Denning
+Author URI: https://github.com/tden123
+License: GPLv2 or later
+Text Domain: wpplugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+include( plugin_dir_path( __FILE__ ) . 'includes/admin-footer-text.php' );
