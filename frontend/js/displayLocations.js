1989a78bd4adb2ff7ba2bd16ef81dad23a8a6b68
Terence Denning <terencedenning@outlook.com>
2021-06-08 22:26:48 +1000

    updates

--- frontend/js/displayLocations.js
+++ frontend/js/displayLocations.js
@@ -3,8 +3,8 @@
     postcode_data format: [
       town: String, 
       postcode: String, 
-      run: String, area: 
-      String, 
+      run: String, 
+      area: String, 
       days_available: Array
     ] 
   */
@@ -19,34 +19,35 @@
 
   function handleInput(e) {
     displayData.innerHTML = '';
-
     // add table header
     if (postcode_data.length > 0 && e.target.value.length > 1) {
       displayData.innerHTML += `
       <tr id='postcodes__data-header'>
         <th>Town</th>
         <th>Postcode</th>
-        <th>Run</th>
-        <th>Area</th>
+        <th class="hide-on-mobile">Area</th>
         <th>Delivery day/s</th>
       <tr>`;
     }
 
     if (e.target.value.length > 1) {
+        
       // add data points
       postcode_data.forEach((item) => {
         if (
-          (item[0].toLowerCase().includes(e.target.value.toLowerCase()) ||
-            item[1].toLowerCase().includes(e.target.value.toLowerCase())) &&
-          e.target.value.length > 0
+          (
+            item[0] && 
+            item[1] &&
+            e.target.value.length > 0 &&
+            (item[0].toLowerCase().startsWith(e.target.value.toLowerCase()) ||
+            item[1].toLowerCase().startsWith(e.target.value.toLowerCase()))) 
         ) {
           displayData.innerHTML += `
       <tr>
         <td>${item[0]}</td>
         <td>${item[1]}</td>
-        <td>${item[2]}</td>
-        <td>${item[3]}</td>
-        <td>${item[4].join(', ')}</td>
+        <td class="hide-on-mobile">${item[2]}</td>
+        <td>${item[3].join(', ')}</td>
       </tr>`;
         }
       });
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,28 +1,47 @@
 <?php
 
-$args = array( 'post_type' => 'postcodes', 'fields' => 'ids' );
+add_action( 'template_redirect', 'plugin_is_page' );
 
-$postcode_ids = get_posts( $args );
+function plugin_is_page() {
+    if ( is_page( '5309' ) ) {  
 
-$postcode_data = [];
+        $args = array( 'post_type' => 'postcodes', 'fields' => 'ids', 'numberposts' => -1 );
 
-foreach( $postcode_ids as $id ) {
-    $town = get_field('town', $id);
-    $postcode = get_field('postcode', $id);
-    $run = get_field('run', $id);
-    $area = get_field('area', $id);
-    $days_available = get_field('days_available', $id);
+        $postcode_ids = get_posts( $args );
 
-    $data = [];
-    array_push($data, $town);
-    array_push($data, $postcode);
-    array_push($data, $run);
-    array_push($data, $area);
-    array_push($data, $days_available);
+        $postcode_data = [];
 
-    array_push($postcode_data, $data);
+        foreach( $postcode_ids as $id ) {
+            $town = get_field('town', $id);
+            $postcode = get_field('postcode', $id);
+            $area = get_field('area', $id);
+            $days_available = get_field('days_available', $id);
+            
+            if($town == null) {
+                $town = "Unknown";
+            }
+            if($postcode == null) {
+                $postcode = "Unknown";
+            }
+            if($area == null) {
+                $area = "Unknown";
+            }
+            if($days_available == null) {
+                $days_available = "Unknown";
+            }
+
+            $data = [];
+            array_push($data, $town);
+            array_push($data, $postcode);
+            array_push($data, $area);
+            array_push($data, $days_available);
+
+            // push data to 2D array
+            array_push($postcode_data, $data);
+        }
+        
+        wp_register_script( 'postcodes', WPPLUGIN_URL . 'frontend/js/displayLocations.js', [], false, true );
+        wp_localize_script('postcodes', 'postcode_data', $postcode_data);
+        wp_enqueue_script( 'postcodes' );
+    }
 }
-
-wp_register_script( 'postcodes', WPPLUGIN_URL . 'frontend/js/displayLocations.js', [], false, true );
-wp_localize_script('postcodes', 'postcode_data', $postcode_data);
-wp_enqueue_script( 'postcodes' );
